ec493ddcdf61b4ea9c68e6e91ff39b0029e5cc8a
funboyshree <85497935+funboyshree@users.noreply.github.com>
2022-02-11 22:14:15 +0530

    Add files via upload

---
+++ Letschat_room.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHtzsfvpIIj72A8pdKvpTWrTu-DLEjHtM",
+  authDomain: "great-d1145.firebaseapp.com",
+  databaseURL: "https://great-d1145-default-rtdb.firebaseio.com",
+  projectId: "great-d1145",
+  storageBucket: "great-d1145.appspot.com",
+  messagingSenderId: "1081704411258",
+  appId: "1:1081704411258:web:bca520f991016c05bfcbc3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
